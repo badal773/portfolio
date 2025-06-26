9382318f3c86a8414754c81ba701eaac7c3ccbf7
Badal Kumar Prusty <badal@devtron.ai>
2025-06-26 19:59:04 +0530

    fix: porfolio image path

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: '/portfolio/',
-  basePath: '/portfolio',
-  output: 'export'
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  output: 'export',
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? '/portfolio' : '',
+  },
 };
-
 
 export default nextConfig;
